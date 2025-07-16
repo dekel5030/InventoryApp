96885cc92e52db17c383ff57574dd27ba6c08544
Dekel5030 <dekel5030@gmail.com>
2025-07-16 14:40:22 +0300

    Added db pool into production database

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,17 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// module.exports = pool;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.PRODUCTION_DATABASE_URL,
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
 module.exports = pool;
